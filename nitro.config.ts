16a98fce8a963c02074cc821c065b3b7cefc4e76
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-03-21 19:53:01 -0700

    add runtime config for redis

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,11 +1,13 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   // srcDir: "server",
-  serverAssets: [{
-    baseName: 'templates',
-    dir: './templates'
-  }],
-  noPublicDir: false,  // default
+  serverAssets: [
+    {
+      baseName: "templates",
+      dir: "./templates",
+    },
+  ],
+  noPublicDir: false, // default
   publicAssets: [
     {
       baseURL: "images",
@@ -13,5 +15,9 @@ export default defineNitroConfig({
       maxAge: 60 * 60 * 24 * 7, // 7 days
     },
   ],
-  compressPublicAssets: { gzip: true, brotli: true }
+  compressPublicAssets: { gzip: true, brotli: true },
+  runtimeConfig: {
+    upstashRedisRestUrl: process.env.NUXT_UPSTASH_REDIS_REST_URL,
+    upstashRedisRestToken: process.env.NUXT_UPSTASH_REDIS_REST_TOKEN,
+  },
 });
