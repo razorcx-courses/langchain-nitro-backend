72225a098a1f7fa90089c19a80246399d539e598
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-03-23 13:27:06 -0700

    add middleware auth validation

---
+++ middleware/request.js
@@ -0,0 +1,15 @@
+export default defineEventHandler((event) => {
+  const authCode = getRequestHeader(event, "Authorization");
+
+  console.log("Middleware | URL: " + getRequestURL(event));
+  console.log("Middleware | EVENT: " + event);
+  console.log("Middleware | Authorization: " + authCode);
+
+  //check authorization
+  const { apiKey } = useRuntimeConfig();
+  if (authCode !== apiKey)
+    return createError({
+      statusCode: 401,
+      statusMessage: "Unauthorized",
+    });
+});
