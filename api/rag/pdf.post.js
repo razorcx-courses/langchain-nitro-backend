bd2a8c9b2fc7a2fdc3eecf1d8a5ca1464d38e321
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-03-24 12:32:10 -0700

    add notes and remove unused code

--- api/rag/pdf.post.js
+++ api/rag/pdf.post.js
@@ -3,11 +3,14 @@ import { useRagWithPdf } from "../../lib/ragWithPdf";
 export default defineEventHandler(async (event) => {
   try {
     const { docs, prompt } = await readBody(event);
-    return await useRagWithPdf(docs, prompt);
+    console.log(docs, prompt);
+    const response = await useRagWithPdf(docs, prompt);
+    console.log(response);
+    return response;
   } catch (e) {
     return createError({
       statusCode: 500,
-      statusMessage: "Server error"
+      statusMessage: "Server error",
     });
   }
 });
